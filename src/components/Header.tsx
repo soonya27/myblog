3ff4a9e648aee45368fec191b02a3713ea937a46
soyeong <psykor48@gmail.com>
2024-04-26 13:52:48 +0900

    header css 수정정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ const menuList = [
 export default function Header() {
     const pathname = usePathname();
     return (
-        <header className=' py-3 px-5 bg-main-blue dark:bg-black'>
+        <header className='py-3 px-2 sm:px-5 bg-main-blue dark:bg-black'>
             <div className='flex justify-between items-center max-w-screen-2xl mx-auto'>
                 <h1 className='font-bold'>
                     <Link href="/">
@@ -24,10 +24,10 @@ export default function Header() {
                     </Link>
                 </h1>
                 <nav>
-                    <ul className={`flex gap-4 [&>*]:text-white [&>*]:font-bold hover:[&>*]:text-black`}>
+                    <ul className={`flex gap-1 sm:gap-4 [&>*]:text-white [&>*]:font-bold hover:[&>*]:text-black`}>
                         {
                             menuList.map(li => (
-                                <li key={li.text} className='relative' ><Link className={`${pathname.includes(li.href) && 'text-black after:content-[""] after:w-[0.35rem] after:h-[0.35rem] after:bg-main-pink after:block after:rounded-full after:absolute after:top-0 after:-right-1'} text-xl dark:hover:text-slate-300 ${pathname.includes(li.href) ? 'dark:text-slate-200' : 'dark:text-slate-400'}`} href={li.href}>{li.text}</Link></li>
+                                <li key={li.text} className='relative' ><Link className={`${pathname.includes(li.href) && 'text-black after:content-[""] after:w-[0.35rem] after:h-[0.35rem] after:bg-main-pink after:block after:rounded-full after:absolute after:top-0 after:-right-1'} text-sm sm:text-xl dark:hover:text-slate-300 ${pathname.includes(li.href) ? 'dark:text-slate-200' : 'dark:text-slate-400'}`} href={li.href}>{li.text}</Link></li>
                             ))
                         }
                     </ul>
